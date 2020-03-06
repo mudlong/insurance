e9492f0f2058527a5d0d885fd2f1207301049d9e
mudlong <38218449+mudlong@users.noreply.github.com>
2020-03-06 15:42:11 +0100

    Add files via upload

--- aeinsure.js
+++ aeinsure.js
@@ -1,6 +1,8 @@
 const contractSource =`
 contract Insureance = 
 	
+	type s = string
+	
     record insure =
         { conAddress  : address,
           fName       : string,
@@ -31,26 +33,26 @@ contract Insureance =
     entrypoint getinsuredLength() : int =
         state.insuredLength
     
-    entrypoint alreadyInsured(ident : string) : bool = 
+    entrypoint alreadyInsured(ident :  s) : bool = 
         Map.member(ident , state.insurer)
         
     entrypoint getinsurePerson(id1:string) = 
         require(alreadyInsured(id1),"person is not Insured")
         state.insurer[id1]
          
-    stateful entrypoint createInsureance(fName':string, lName':string,  id1':string, idd':int,phone': int, email':string,
-                                    address':string, city':string, state':string, fName2':string,
-                                    lName2':string,email2':string,relation':string, phone2':int) =
+    stateful entrypoint createInsureance(fName': s, lName': s,  id1': s, idd':int,phone': int, email': s,address': s, city': s, 
+  										 state': s, fName2': s, lName2': s,email2': s,relation': s, phone2':int) =
         require(!alreadyInsured(id1'),"person is already Insured")
         insureDetails(fName',lName',id1',idd',phone',email',address', city', state',fName2', lName2',email2', relation', phone2')
           
           
-    stateful function insureDetails(fName':string ,lName':string, id1':string , idd': int,phone': int, email':string,
-                                    address':string, city':string, state':string, fName2':string,
-                                    lName2':string,email2':string,relation':string, phone2':int) =
-        let person = { conAddress = Call.caller, fName = fName', lName = lName', id1 = id1',idd = idd',
-                       phone = phone', email = email', address = address', city=city', state=state', fName2=fName2',
-                       lName2 = lName2',email2 = email2',relation= relation', phone2=phone2'}
+    stateful function insureDetails(fName': s ,lName': s, id1': s , idd': int,phone': int, email': s,address': s, city': s, state': s,
+  								    fName2': s, lName2': s,email2': s,relation': s, phone2':int) =
+        let person = { conAddress = Call.caller, fName = fName', lName = lName',
+        			   id1 = id1',idd = idd', phone = phone', email = email',
+        			   address = address', city=city', state=state',
+        			   fName2=fName2',lName2 = lName2',email2 = email2',
+        			   relation= relation', phone2=phone2'}
                        
         let index  = getinsuredLength() + 1
         put( state {insurer[id1'] = person, insuredLength = index}) 
@@ -64,33 +66,70 @@ var client = null;
 var insuredLength = 0;
 var insureArray = [];
 
-//Create a asynchronous read call for our smart contract
+// Create a asynchronous read call for our smart contract
+// async function callStatic(func, args) {
+//   //Create a new contract instance that we can interact with
+//   const contract = await client.getContractInstance(contractSource, {contractAddress});
+//   //Make a call to get data of smart contract func, with specefied arguments
+//   console.log('args', args);
+//   console.log('func', func);
+//   const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
+//   console.log('calledGet', calledGet);
+//   //Make another call to decode the data received in first call
+//   const decodedGet = await calledGet.decode().catch(e => console.error(e));
+
+//   return decodedGet;
+// }
+
+// //Create a asynchronous write call for our smart contract
+// async function contractCall(func, args, value) {
+//   const contract = await client.getContractInstance(contractSource, {contractAddress});
+//   //Make a call to write smart contract func, with aeon value input
+//   const calledSet = await contract.call(func, args, {amount: value}).catch(e => console.error(e));
+//   return calledSet;
+// }
+
 async function callStatic(func, args) {
   //Create a new contract instance that we can interact with
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
-  //Make a call to get data of smart contract func, with specefied arguments
-  console.log('args', args);
-  console.log('func', func);
-  const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
-  console.log('calledGet', calledGet);
-  //Make another call to decode the data received in first call
-  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
 
+  const calledGet = await contract
+    .call(func, args, {
+      callStatic: true
+    })
+    .catch(e => console.error(e));
+
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
+  console.log("number of posts : ", decodedGet);
   return decodedGet;
 }
 
-//Create a asynchronous write call for our smart contract
 async function contractCall(func, args, value) {
-  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const contract = await client.getContractInstance(contractSource, {
+    contractAddress
+  });
   //Make a call to write smart contract func, with aeon value input
-  const calledSet = await contract.call(func, args, {amount: value}).catch(e => console.error(e));
+  const calledSet = await contract
+    .call(func, args, {
+      amount: value
+    })
+    .catch(e => console.error(e));
+
   return calledSet;
 }
+
+
+
+
+
+
 
 // $("#regBtn")
 // .click(async function () {
 document.getElementById('regBtn').addEventListener('click', async function(){
-
+$('#loader').show();
 
     var ele = document.getElementsByName('gender');     
      for(i = 0; i < ele.length; i++) { 
@@ -126,7 +165,7 @@ document.getElementById('regBtn').addEventListener('click', async function(){
 }
 		  // alert(gendar)
 
-await contractCall('createInsureance', [fName, lName, id1, phone,
+const write = await contractCall("createInsureance", [fName, lName, id1, phone,
 								  email, address, city, state, fName2, lName2,
 								  phone2, email2, relation], 0);
 
@@ -152,7 +191,8 @@ await contractCall('createInsureance', [fName, lName, id1, phone,
 		  	index : insureArray.length + 1
 
 		})
-		  console.log("Insurance completed ")
+		  $('#loader').hide();
+		  console.log("Insurance completed ");
 		  // renderInsure();
 })
 // $('#regHos').click(async function(){
